9f6c4de1023460a3ced1865815900bd23be006db
nanqiu <hanwen.zhang@vanderbilt.edu>
2024-08-01 19:28:50 -0500

    initial commit

---
+++ inventory_management/firebase.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB7f_y8DrwwVwOucFvRqHyTaAEKTYCDb5k",
+  authDomain: "inventory-management-bd60a.firebaseapp.com",
+  projectId: "inventory-management-bd60a",
+  storageBucket: "inventory-management-bd60a.appspot.com",
+  messagingSenderId: "730156428004",
+  appId: "1:730156428004:web:6a596be919dedc9098daf1",
+  measurementId: "G-9WJ9GCCSXP"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const firestore = getFirestore(app);
+
+export { firestore }
